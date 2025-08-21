754f52157fb77b75eea55bf4aa14c9bdb07007bc
-akku- <akkun11.open@gmail.com>
2025-08-22 00:41:07 +0900

    wip: mt6589 working

--- drivers/clk/mediatek/clk-mt6589.c
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: GPL-2.0-or-later
-/*
- * MediaTek clock driver for MT6589 SoC
- *
- * Copyright (C) 2025 akku
- * Author: akku <akkun11.open@gmail.com>
- */
-
-/* TODO */
