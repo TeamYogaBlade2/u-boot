686043b72d435d15a4ede709571d73ce87ac5c8c
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-03-25 18:59:49 +0900

    for debug
    Signed-off-by: Akari Tsuyukusa <akkun11.open@gmail.com>

--- board/mediatek/mt6589/mt6589_board.c
+++ board/mediatek/mt6589/mt6589_board.c
@@ -3,7 +3,17 @@
  * Copyright (C) 2026 Akari Tsuyukusa
  */
 
+#include <cpu_func.h>
+#include <linux/string.h>
+#include <linux/io.h>
+
 int board_init(void)
 {
+	memset((void *)0xbf600000, 0x33, 0x400000);
+	flush_dcache_range(0xbf600000, 0xbfa00000);
+
+	printf("Hello World!");
+	flush_dcache_range(0xbf600000, 0xbfa00000);
+
 	return 0;
 }
